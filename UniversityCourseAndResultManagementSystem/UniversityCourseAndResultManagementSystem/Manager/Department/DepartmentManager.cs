628cbdd7affb9f04f2b55dffe3316ed7d55979ee
Muhammad Nurul Momen <mdnmomen@gmail.com>
2021-07-01 14:07:49 +0600

    first commit

---
+++ UniversityCourseAndResultManagementSystem/UniversityCourseAndResultManagementSystem/Manager/Department/DepartmentManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultManagementSystem.Gateway.Department;
+
+namespace UniversityCourseAndResultManagementSystem.Manager.Department
+{
+    public class DepartmentManager
+    {
+        DepartmentGateway departmentGateway = new DepartmentGateway();
+
+        public string SaveDepartment(Models.Department department)
+        {
+            //if (departmentGateway.IsDepartmentNameOrCodeExist(department.Code, department.DepartmentName))
+            //{
+            //    return "Name/Code Already Exist";
+            //}
+
+            if (departmentGateway.SaveDepartment(department)>0)
+            {
+                return "Department Saved Successfully.";
+            }
+            return "Department saving Failed.";
+          
+        }
+
+
+
+        public List<Models.Department> GetAllDepartments()
+        {
+            return departmentGateway.GetAllDepartments();
+        }
+
+        public string GetDeptCodeById(int id)
+        {
+            return departmentGateway.GetDeptCodeById(id);
+        }
+
+       
+    }
+}
